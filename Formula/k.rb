3f4b0c069722235137eefecef1c9fbcf5a485df3
Vadim Shashin <enapiuz@gmail.com>
2020-05-26 19:55:02 +0300

    add k util

---
+++ Formula/k.rb
@@ -0,0 +1,16 @@
+class K < Formula
+  desc "Exec into kubernetes pod easy (via kubectl)"
+  homepage "https://github.com/yggheim/k"
+  version "0.1.0"
+
+  bottle :unneeded
+
+  url "https://github.com/yggheim/k/archive/master.zip", :using => :curl
+  
+  depends_on "fzf"
+  depends_on "jq"
+
+  def install
+    bin.install "k"
+  end
+end
